5c36c568908b2d315051b777eee2db819876f22f
Pratima Singh <singhpratima21222@gmail.com>
2023-10-12 04:04:26 +0530

    Added context

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { UserContext } from './context';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import Login from './pages/user/login';
 import Signup from './pages/user/signup';
 import Admlogin from './pages/admin/login';
 import Addtherapy from './pages/admin/admAddTherapist';
 import Home from './pages/user/home';
+import Referral from './pages/user/referral'
+import Confirmation from './pages/user/confirmation'
 
 function App() {
-  localStorage.setItem('res', false);
+  const context = useContext(UserContext);
+
   return (
     <ChakraProvider theme={theme}>
       <BrowserRouter>
         <Routes>
+          <Route exact path="/" element={
+            context.active === true ? <Home /> : <Login />
+          } />
+
           <Route path="/user/signup" element={<Signup />} />
           <Route path="/admin/login" element={<Admlogin />} />
           <Route path="/admin/therapist/" element={<Addtherapy />} />
           <Route path="/user/login" element={<Login />} />
-          {localStorage.getItem('res') ? (
-            <Route path="/" element={<Home />} />
-          ) : (
-            <Route exact path="/" element={<Login />} />
-          )}
           <Route path="/user" >
             <Route path="referral" element={<Referral />} />
             <Route path="confirmation" element={<Confirmation />} />
